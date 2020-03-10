b43a8fca4deab40e79ddc4526c665f216c7fea95
Ying-Li Shih <norelle@Nor.local>
2020-03-10 16:50:20 +0800

    Updates

---
+++ precache-manifest.71d9b90bee2ac83fe6d8007a69965b82.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f9dbc0337a9535a9118f81256cda18b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "490959b95e29074752a3",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "3a1d5522a4de7c626402",
+    "url": "/static/css/main.ce5b298e.chunk.css"
+  },
+  {
+    "revision": "490959b95e29074752a3",
+    "url": "/static/js/2.2bd1886f.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.2bd1886f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a1d5522a4de7c626402",
+    "url": "/static/js/main.0c9dc22a.chunk.js"
+  },
+  {
+    "revision": "a00d9fd76856252b14b7",
+    "url": "/static/js/runtime-main.7aa1488f.js"
+  },
+  {
+    "revision": "80ef739b406a2728d88d88c201094ac3",
+    "url": "/static/media/logo.80ef739b.png"
+  },
+  {
+    "revision": "4da9c27cd3a205d5ae8b58f03143189d",
+    "url": "/static/media/profile_photo.4da9c27c.png"
+  }
+]);
